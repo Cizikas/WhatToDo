5470a90a4f99bcc53a54e3e064beb5a1e53ba86a
Cizikas <44546484+Cizikas@users.noreply.github.com>
2019-05-03 01:46:04 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Titles from "./Components/Titles";
 import Form from "./Components/Form";
 import Weather from "./Components/Weather";
 
-const API_KEY = "4c495c89f5ba85bbdd690c7a55518274";
+//const API_KEY = "4c495c89f5ba85bbdd690c7a55518274";
 
 class App extends React.Component {
   state = {
@@ -18,27 +18,48 @@ class App extends React.Component {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
+    const api_id = e.target.elements.api_id.value;
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_id}&units=metric`
     );
     const data = await api_call.json();
-    if (city && country) {
-      this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: ""
-      });
-    } else {
+    try {
+      if (city && country && api_id) {
+        this.setState({
+          temperature: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          humidity: data.main.humidity,
+          description: data.weather[0].description,
+          error: ""
+        });
+      } else if (city && country && !api_id) {
+        this.setState({
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: "Enter the API Key, please"
+        });
+      } else {
+        this.setState({
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: "Enter the values, please"
+        });
+      }
+    } catch (err) {
       this.setState({
         temperature: undefined,
         city: undefined,
         country: undefined,
         humidity: undefined,
         description: undefined,
-        error: "Enter the values, please"
+        error: "Check if api key, city and country are correct"
       });
     }
   };
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,6 +4,8 @@ class Form extends React.Component {
   render() {
     return (
       <form onSubmit={this.props.getWeather}>
+        <input type="text" name="api_id" placeholder="API Key..." />
+        <br />
         <input type="text" name="city" placeholder="city..." />
         <input type="text" name="country" placeholder="country..." />
         <button>Get Weather</button>
